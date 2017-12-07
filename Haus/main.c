6c175f15c0e923cea29867dc587fa14f1ec69673
Leon <Leon.geim@googlemail.com>
2017-12-07 10:37:39 +0100

    Kommentare hinzugefügt

--- Haus/main.c
+++ Haus/main.c
@@ -1,3 +1,11 @@
+/*
+Leon Geim
+
+Problem: Erkennen der Logik, dass es nur 2 sich bewegende Elemente gibt, bei der 1 immer um 1 nach rechts
+und 1 immer um 1 nach links wandert.
+*/
+
+
 #include <stdio.h>
 void main()
 {
@@ -8,9 +16,10 @@ void main()
 	int positionErstesZeichen = 0;
 	int positionZweitesZeichen = breite - 3;
 
-	char zeichenLeer = ' ';
+	char zeichenLeer = '-';
 	char zeichenHaus = 'x';
 
+	//For-Schleife für das Dach
 	for (i = 0; i < breite; i++)
 	{
 		printf("%c", zeichenHaus);
@@ -18,11 +27,13 @@ void main()
 	printf("\n");
 
 
-
+	//Variabler Mittelteil
 	for (i = 1; i <= breite - 2; i++)
 	{
+		//Linke Wand
 		printf("%c", zeichenHaus);
 
+		//breite - 2 da linke und rechte Wand fest sind
 		for (j = 0; j < breite - 2; j++)
 		{
 			if (j == positionErstesZeichen || j == positionZweitesZeichen)
@@ -35,6 +46,7 @@ void main()
 			}
 		}
 
+		//Rechte Wand
 		printf("%c", zeichenHaus);
 		printf("\n");
 
@@ -42,6 +54,7 @@ void main()
 		positionZweitesZeichen--;
 	}
 
+	//For-Schleife für den Boden
 	for (i = 0; i < breite; i++)
 	{
 		printf("%c", zeichenHaus);
